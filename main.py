c6ed50c5d1371412656186df893588c61b509497
Tejas MH <95995054+tejasharish1234@users.noreply.github.com>
2022-04-27 17:05:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ runL = pygame.image.load('Running-02.png')
 runR = pygame.image.load('Running-03.png')
 stand = pygame.image.load('Resting-01.png')
 
+
+
 clock = pygame.time.Clock()
 x = 400
 y = 400
@@ -25,7 +27,6 @@ STAND = False
 
 def charmove():
     global stand1, runR1, runL1
-  
 
     if STAND == True:
         screen.blit(stand1, (x,y))
@@ -51,6 +52,9 @@ def charmove():
         pygame.display.update()
         screen.blit(runR1, (x,y))
 
+    
+
+    
     pygame.display.update()
     
 
@@ -58,59 +62,48 @@ def charmove():
 #game loop
 running = True
 while running == True:
-    
-    
+    clock.tick(30)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False 
-
-        if event.type == pygame.KEYUP:
-            STAND = True
-        
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT :
-                x -= vel
-                left = True
-                runR1 = pygame. transform. rotate(runR, 90)
-                runL1 = pygame. transform. rotate(runL, 90)
-                stand1 = pygame. transform. rotate(stand, 90)
-
-            elif event.key == pygame.K_RIGHT :
-                x += vel
-                right = True
-                runR1 = pygame. transform. rotate(runR, -90)
-                runL1 = pygame. transform. rotate(runL, -90)
-                stand1 = pygame. transform. rotate(stand, -90)
-
-            elif event.key == pygame.K_UP :
-                y -= vel
-                up = True
-                runR1 = pygame. transform. rotate(runR, 0)
-                runL1 = pygame. transform. rotate(runL, 0)
-                stand1 = pygame. transform. rotate(stand, 0)
-
-
-            elif event.key == pygame.K_DOWN  :
-                y += vel
-                down = True
-                runR1 = pygame.transform.rotate(runR, 180)
-                runL1 = pygame.transform.rotate(runL, 180)
-                stand1 = pygame. transform. rotate(stand, 180)
-        
-      
-    if x <= 0 :
-        x = 0
+            running = False
     
-    if y <= 0:
-        y = 0
+   
+    keys = pygame.key.get_pressed()
 
-    if x >= 736:
-        x = 736
+    if keys [pygame.K_LEFT] and x > vel :
+        x -= vel
+        left = True
+        runR1 = pygame. transform. rotate(runR, 90)
+        runL1 = pygame. transform. rotate(runL, 90)
+        stand1 = pygame. transform. rotate(stand, 90)
 
-    if y >= 736:
-        y = 736
+    elif keys [pygame.K_RIGHT] and x < 800 - width - vel :
+        x += vel
+        right = True
+        runR1 = pygame. transform. rotate(runR, -90)
+        runL1 = pygame. transform. rotate(runL, -90)
+        stand1 = pygame. transform. rotate(stand, -90)
 
+    elif keys [pygame.K_UP] and y > vel:
+        y -= vel
+        up = True
+        runR1 = pygame. transform. rotate(runR, 0)
+        runL1 = pygame. transform. rotate(runL, 0)
+        stand1 = pygame. transform. rotate(stand, 0)
+
+    elif keys [pygame.K_DOWN] and y < 800 - width - vel :
+        y += vel
+        down = True
+        runR1 = pygame.transform.rotate(runR, 180)
+        runL1 = pygame.transform.rotate(runL, 180)
+        stand1 = pygame. transform. rotate(stand, 180)
+    
+    if keys [pygame.KEYUP]:
+        STAND = True
+      
     charmove()
+
     screen.fill((0,128,0))
 
 pygame.quit()
